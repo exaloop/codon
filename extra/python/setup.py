0fa861310459302f5be887e474d954e58f37c415
Ishak Numanagić <ishak.numanagic@gmail.com>
2022-03-14 18:53:37 +0100

    Fix CI

--- extra/python/setup.py
+++ extra/python/setup.py
@@ -16,11 +16,11 @@ def get_output(*args):
     return ps.stdout.decode("utf8").strip()
 
 
-from_cwd = lambda relpath: os.path.realpath(f"{os.getcwd()}/{relpath}")
+from_root = lambda relpath: os.path.realpath(f"{os.getcwd()}/../../{relpath}")
 
 
 llvm_config: str
-llvm_config_candidates = ["llvm-config-12", "llvm-config", "llvm/bin/llvm-config"]
+llvm_config_candidates = ["llvm-config-12", "llvm-config", from_root("llvm/bin/llvm-config")]
 for candidate in llvm_config_candidates:
     if exists(candidate):
         llvm_config = candidate
@@ -31,30 +31,28 @@ else:
 llvm_include_dir = get_output(llvm_config, "--includedir")
 llvm_lib_dir = get_output(llvm_config, "--libdir")
 
-codon_include_dir = os.environ.get("CODON_INCLUDE_DIR", from_cwd("../../build/include"))
-codon_lib_dir = os.environ.get("CODON_LIB_DIR", from_cwd("../../build"))
+codon_include_dir = os.environ.get("CODON_INCLUDE_DIR", from_root("build/include"))
+codon_lib_dir = os.environ.get("CODON_LIB_DIR", from_root("build"))
 
 print(f"<llvm>  {llvm_include_dir}, {llvm_lib_dir}")
 print(f"<codon> {codon_include_dir}, {codon_lib_dir}")
 
-extensions = [
-    Extension(
-        "codon_jit",
-        sources=["src/jit.pyx"],
-        libraries=["codonc", "codonrt"],
-        language="c++",
-        extra_compile_args=["-w", "-std=c++17"],
-        extra_link_args=[f"-Wl,-rpath,{codon_lib_dir}"],
-        include_dirs=[llvm_include_dir, codon_include_dir],
-        library_dirs=[llvm_lib_dir, codon_lib_dir],
-    )
-]
+jit_extension = Extension(
+    "codon_jit",
+    sources=["src/jit.pyx"],
+    libraries=["codonc", "codonrt"],
+    language="c++",
+    extra_compile_args=["-w", "-std=c++17"],
+    extra_link_args=[f"-Wl,-rpath,{codon_lib_dir}"],
+    include_dirs=[llvm_include_dir, codon_include_dir],
+    library_dirs=[llvm_lib_dir, codon_lib_dir],
+)
 
 setup(
     name="codon",
     version="0.1.0",
     cmdclass={"build_ext": build_ext},
-    ext_modules=extensions,
+    ext_modules=[jit_extension],
     packages=["codon"],
     package_dir={"codon": "src"}
 )
