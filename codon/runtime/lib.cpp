884c949b7fef82fa92d5cbf61858066a06ffadf4
A. R. Shajii <ars@ars.me>
2022-07-23 14:15:49 -0400

    Fix side-effect analysis for try-catch

--- codon/runtime/lib.cpp
+++ codon/runtime/lib.cpp
@@ -24,7 +24,7 @@
 
 using namespace std;
 
-SEQ_FUNC int seq_test_global_var = 42;  // used for testing extern C variables
+SEQ_FUNC int seq_test_global_var = 42; // used for testing extern C variables
 
 /*
  * General
