c91967b7dabaa5a7b845228eb46f9741ad60b52c
A. R. Shajii <ars@ars.me>
2022-08-21 16:31:30 -0400

    Fix global renaming

--- codon/sir/llvm/gpu.cpp
+++ codon/sir/llvm/gpu.cpp
@@ -431,8 +431,7 @@ void moduleToPTX(llvm::Module *M, const std::string &filename,
   // Clean up names.
   {
     for (auto &G : M->globals()) {
-      if (G.hasLocalLinkage())
-        G.setName(cleanUpName(G.getName()));
+      G.setName(cleanUpName(G.getName()));
     }
 
     for (auto &F : M->functions()) {
