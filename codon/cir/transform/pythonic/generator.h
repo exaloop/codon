810cb33dbba9f654f8b31489c50324ba7d5133c2
A. R. Shajii <ars@ars.me>
2023-01-13 15:27:05 -0500

    Add generator-argument optimization

---
+++ codon/cir/transform/pythonic/generator.h
@@ -0,0 +1,25 @@
+// Copyright (C) 2022-2023 Exaloop Inc. <https://exaloop.io>
+
+#pragma once
+
+#include "codon/cir/transform/pass.h"
+
+namespace codon {
+namespace ir {
+namespace transform {
+namespace pythonic {
+
+/// Pass to optimize passing a generator to some built-in functions
+/// like sum(), any() or all(), which will be converted to regular
+/// for-loops.
+class GeneratorArgumentOptimization : public OperatorPass {
+public:
+  static const std::string KEY;
+  std::string getKey() const override { return KEY; }
+  void handle(CallInstr *v) override;
+};
+
+} // namespace pythonic
+} // namespace transform
+} // namespace ir
+} // namespace codon
