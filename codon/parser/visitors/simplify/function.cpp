a86a24a79fb02076d90a00aeb9cc9670e1c60bbf
A. R. Shajii <ars@ars.me>
2023-01-13 17:50:35 -0500

    Fix typo

--- codon/parser/visitors/simplify/function.cpp
+++ codon/parser/visitors/simplify/function.cpp
@@ -322,7 +322,7 @@ ExprPtr SimplifyVisitor::makeAnonFn(std::vector<StmtPtr> suite,
     prependStmts->push_back(fs->stmts[0]);
     for (StmtPtr s = fs->stmts[1]; s;) {
       if (auto suite = s->getSuite()) {
-        // Suites can only occur when captrues are inserted for a partial call
+        // Suites can only occur when captures are inserted for a partial call
         // argument.
         seqassert(suite->stmts.size() == 2, "invalid function transform");
         prependStmts->push_back(suite->stmts[0]);
