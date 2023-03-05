7fbf877a6dedcc176a7652c54fb05ab092dd288f
Ibrahim Numanagić <ibrahimpasa@gmail.com>
2023-03-04 20:04:48 -0800

    Refactor out Jupyter support

---
+++ codon/util/jupyter.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2022-2023 Exaloop Inc. <https://exaloop.io>
+
+#include "codon/util/jupyter.h"
+#include <cstdio>
+
+namespace codon {
+int startJupyterKernel(const std::string &argv0,
+                       const std::vector<std::string> &plugins,
+                       const std::string &configPath) {
+  fprintf(stderr, "Jupyter support not included. Please install Codon Jupyter plugin.\n");
+  return EXIT_FAILURE;
+}
+
+} // namespace codon
---
+++ jupyter/jupyter.h
@@ -0,0 +1,43 @@
+// Copyright (C) 2022 Exaloop Inc. <https://exaloop.io>
+
+#pragma once
+#include <codon/compiler/jit.h>
+#include <nlohmann/json.hpp>
+#include <xeus/xinterpreter.hpp>
+
+using xeus::xinterpreter;
+namespace nl = nlohmann;
+
+namespace codon {
+class CodonJupyter : public xinterpreter {
+  std::unique_ptr<codon::jit::JIT> jit;
+  std::string argv0;
+  std::vector<std::string> plugins;
+
+public:
+  CodonJupyter(const std::string &argv0, const std::vector<std::string> &plugins);
+
+private:
+  void configure_impl() override;
+
+  nl::json execute_request_impl(int execution_counter, const std::string &code,
+                                bool silent, bool store_history,
+                                nl::json user_expressions, bool allow_stdin) override;
+
+  nl::json complete_request_impl(const std::string &code, int cursor_pos) override;
+
+  nl::json inspect_request_impl(const std::string &code, int cursor_pos,
+                                int detail_level) override;
+
+  nl::json is_complete_request_impl(const std::string &code) override;
+
+  nl::json kernel_info_request_impl() override;
+
+  void shutdown_request_impl() override;
+};
+
+int startJupyterKernel(const std::string &argv0,
+                       const std::vector<std::string> &plugins,
+                       const std::string &configPath);
+
+} // namespace codon
