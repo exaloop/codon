793cf173a8fa60bc85f8c6bb0685c2991bf1a72a
A. R. Shajii <ars@ars.me>
2026-09-23 21:03:14 -0400

    Fix getopt module

--- test/main.cpp
+++ test/main.cpp
@@ -484,6 +484,7 @@ INSTANTIATE_TEST_SUITE_P(
         "stdlib/math_test.codon",
         "stdlib/cmath_test.codon",
         "stdlib/datetime_test.codon",
+        "stdlib/getopt_test.codon",
         "stdlib/itertools_test.codon",
         "stdlib/json_test.codon",
         "stdlib/bisect_test.codon",
